087830deddd4f59464c6f8d9c4bcf5c6ca8c0aa6
msv1990 <htconemailbox@gmail.com>
2019-08-05 11:24:22 +0300

    fix:fixer fixed

--- src/components/ArchitectPage/Gallery/Gallery.jsx
+++ src/components/ArchitectPage/Gallery/Gallery.jsx
@@ -1,29 +1,25 @@
-// import React from 'react';
+import React from 'react';
 
-// import NoSSR from 'react-no-ssr';
+import idGenerator from 'react-id-generator';
 
-// import AwesomeSlider from 'react-awesome-slider';
+import AwesomeSlider from './react-awesome-slider/dist/index';
 
-// import idGenerator from 'react-id-generator';
+import AwsSliderStyles from './styles.scss';
 
-// import AwsSliderStyles from './styles.scss';
-
-// import style from './style.module.css';
+import style from './style.module.css';
 
 
-// const gallery = ({ work }) => {
-//   const data = Object.values(JSON.parse(work.internal.content));
-//   const worksImg = data.map(el => (
-//     <div key={idGenerator()} data-src={el.img}>
-//       <p>{el.title}</p>
-//     </div>
-//   ));
-//   return (
-//     <NoSSR>
-//       <AwesomeSlider cssModule={AwsSliderStyles} className={style.Container}>
-//         {worksImg}
-//       </AwesomeSlider>
-//     </NoSSR>
-//   );
-// };
-// export default gallery;
+const gallery = ({ work }) => {
+  const data = Object.values(JSON.parse(work.internal.content));
+  const worksImg = data.map(el => (
+    <div key={idGenerator()} data-src={el.img}>
+      <p>{el.title}</p>
+    </div>
+  ));
+  return (
+    <AwesomeSlider cssModule={AwsSliderStyles} className={style.Container}>
+      {worksImg}
+    </AwesomeSlider>
+  );
+};
+export default gallery;
--- src/components/ArchitectPage/architectPage.jsx
+++ src/components/ArchitectPage/architectPage.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 
-import NoSSR from 'react-no-ssr';
-
 import Layout from '../Layout/Layout';
 
 import Activity from './Activity/Activity';
-// import Gallery from './Gallery/Gallery';
-import SimpleMap from './Map/Map';
+import Map from './Map/Map';
 import Person from './Person/Person';
 import Timeline from './Timeline/Timeline';
 import Video from './Video/Video';
+import Gallery from './Gallery/Gallery';
+
+import style from './style.module.css';
 
 class Architect extends Component {
   constructor(props) {
@@ -34,14 +34,13 @@ class Architect extends Component {
     } = this.data;
     return (
       <Layout>
-        <div>
+        <div className={style.Container}>
           <Person name={name} img={img} vita={vita} date={date} />
           <Timeline timelineData={timelineData} />
           <Activity work={work} />
           <Video videoId={videoId} />
-          <NoSSR>
-            <SimpleMap work={work} />
-          </NoSSR>
+          <Map work={work} />
+          <Gallery work={work} />
         </div>
       </Layout>
     );
